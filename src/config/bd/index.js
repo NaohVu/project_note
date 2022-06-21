3d74749a1c762b57963a8008c22d20fc0f4165d4
naoh vu <hoanpoint@gmail.com>
2022-06-21 16:58:14 +0700

    a

--- src/config/bd/index.js
+++ src/config/bd/index.js
@@ -1,15 +1,23 @@
 const mysql = require('mysql')
 require('dotenv').config()
 
+// const con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "notes",
+
+
+// })
+
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "notes",
+  host: "bbeaj1mkry1wekdffyhz-mysql.services.clever-cloud.com",
+  user: "uj4foh17ffbafcg4",
+  password: "uj4foh17ffbafcg4",
+  database: "bbeaj1mkry1wekdffyhz",
 
 
 })
-
 console.log('sssss')
 
 

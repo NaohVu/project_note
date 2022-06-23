ec067fdf9d02d13169cd2f9765f4fbb401b6cbf3
naoh vu <hoanpoint@gmail.com>
2022-06-23 12:16:40 +0700

    finish

--- src/config/bd/index.js
+++ src/config/bd/index.js
@@ -1,24 +1,16 @@
 const mysql = require('mysql')
 require('dotenv').config()
 
-// const con = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "notes",
-
-
-// })
-
 const con = mysql.createConnection({
-  host: "bbeaj1mkry1wekdffyhz-mysql.services.clever-cloud.com",
-  user: "uj4foh17ffbafcg4",
-  password: "Zx1ArHNrBg3iwnEDWZ3s",
-  database: "bbeaj1mkry1wekdffyhz",
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "notes",
 
 
 })
-console.log('sssss')
+
+
 
 con.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
   if (error) throw error;
